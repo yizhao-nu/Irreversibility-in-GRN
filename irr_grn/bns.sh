363d4c6e526388cd88a1b20cb2221dadaac8c5ca
Thomas P Wytock <thomaspwytock@dhcp-10-105-180-91.wireless.northwestern.private>
2023-08-08 14:09:42 -0500

    fixed file pathing for bns

--- irr_grn/bns.sh
+++ irr_grn/bns.sh
@@ -6,9 +6,9 @@ for o in ${ord[@]} do
         for i in {0..9}; do
             printf -v ii '%2d' i
             printf -v pp '%.2f' p
-            nohup ./bns newneg2_rs2_${pp}_${o}_${ii}_1.cnet> att_newneg2_rs2_${pp}_${o}_${ii}_1.txt 2>&1 & 
+            nohup ./bns ./netfiles/newneg2_rs2_${pp}_${o}_${ii}_1.cnet> ./attfiles/att_newneg2_rs2_${pp}_${o}_${ii}_1.txt 2>&1 & 
     done
-    nohup ./bns newneg2_rs2_0.00_${o}_00_1.cnet> att_newneg2_rs2_0.00_${o}_00_1.txt 2>&1 &
-    nohup ./bns newneg2_rs2_1.00_${o}_00_1.cnet> att_newneg2_rs2_1.00_${o}_00_1.txt 2>&1 &
+    nohup ./bns ./netfiles/newneg2_rs2_0.00_${o}_00_1.cnet> ./attfiles/att_newneg2_rs2_0.00_${o}_00_1.txt 2>&1 &
+    nohup ./bns ./netfiles/newneg2_rs2_1.00_${o}_00_1.cnet> ./attfiles/att_newneg2_rs2_1.00_${o}_00_1.txt 2>&1 &
 done
 
