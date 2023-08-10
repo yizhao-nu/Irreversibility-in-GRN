191b2494999866725881f44212219ce574478a80
Thomas P Wytock <thomaspwytock@dhcp-10-105-180-91.wireless.northwestern.private>
2023-08-10 14:52:46 -0500

    corrected bns.sh

--- irr_grn/bns.sh
+++ irr_grn/bns.sh
@@ -4,11 +4,11 @@ ord=("asc" "desc")
 for o in ${ord[@]}; do
     for p in ${ps[@]}; do
         for i in {0..9}; do
-            printf -v ii '%2d' i
+            printf -v ii '%02d' i
             printf -v pp '%.2f' p
-            nohup ./bns ./netfiles/newneg2_rs2_${pp}_${o}_${ii}_1.cnet> ./attfiles/att_newneg2_rs2_${pp}_${o}_${ii}_1.txt 2>&1 & 
+            nohup ./bns "./netfiles/newneg2_rs2_${pp}_${o}_${ii}_1.cnet" > "./attfiles/att_newneg2_rs2_${pp}_${o}_${ii}_1.txt"
         done
-    nohup ./bns ./netfiles/newneg2_rs2_0.00_${o}_00_1.cnet> ./attfiles/att_newneg2_rs2_0.00_${o}_00_1.txt 2>&1 &
-    nohup ./bns ./netfiles/newneg2_rs2_1.00_${o}_00_1.cnet> ./attfiles/att_newneg2_rs2_1.00_${o}_00_1.txt 2>&1 &
+    nohup ./bns "./netfiles/newneg2_rs2_0.00_${o}_00_1.cnet"> "./attfiles/att_newneg2_rs2_0.00_${o}_00_1.txt"
+    nohup ./bns "./netfiles/newneg2_rs2_1.00_${o}_00_1.cnet"> "./attfiles/att_newneg2_rs2_1.00_${o}_00_1.txt"
     done
 done
