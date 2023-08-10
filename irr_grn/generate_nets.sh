5b21f79fbd46be1fadfdb0d2d74488ab80d0374f
yizhao-nu <137353167+yizhao-nu@users.noreply.github.com>
2023-08-10 14:40:41 -0500

    Add files via upload

--- irr_grn/generate_nets.sh
+++ irr_grn/generate_nets.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-ps=(0.2 0.4 0.6 0.8)
+ps=(0.20 0.40 0.60 0.80)
 orders=('asc' 'desc')
 for order in ${orders[@]}; do
 for p in ${ps[@]}; do
-for i in {0..9}; do
+for i in 0{0..9}; do
 nohup python3 generate_nets.py ${p} ${order} ${i}> gennet_${p}_${order}_${i}.txt 2>&1 & 
 done
 done
-nohup python3 generate_nets.py 0 ${order} 0> gennet_${p}_${order}_${i}.txt 2>&1 & 
-nohup python3 generate_nets.py 1 ${order} 0> gennet_${p}_${order}_${i}.txt 2>&1 & 
+nohup python3 generate_nets.py 0.00 ${order} 0> gennet_${p}_${order}_${i}.txt 2>&1 & 
+nohup python3 generate_nets.py 1.00 ${order} 0> gennet_${p}_${order}_${i}.txt 2>&1 & 
 done
