8af2cadcd1f3ae877204ed13ad14bd22b585056c
twytock <twytock@gmail.com>
2023-12-07 23:35:37 -0600

    Fixed errors in bns.sh and updated README.md

--- irr_grn/README.md
+++ irr_grn/README.md
@@ -9,4 +9,13 @@ Run the command `./generate_nets.sh`.
 This will generate network files in the `netfiles/` directory.
 There should be 20 for each value of $r$ and $s$ considered in the manuscript (except those that lead to unique rules). 
 
+## Find attractors
+Run the command `./bns.sh`.
+This will generate the attractors, which are stored in the `attfiles/` directory.
+
+Run the command `./attcsv.sh`.
+This will generate the files that contain the first states of the attractors, which will be used to detect irreversibility.
+
+## Run the irreversibility detection algorithm
+
 
--- irr_grn/bns.sh
+++ irr_grn/bns.sh
@@ -6,15 +6,15 @@ for order in ${ord[@]}; do
         q1=$(bc -l <<<"1-${p}")
         printf -v q11 "%.02f" ${q1}
         printf -v pp '%.02f' ${p}
-        for i in {00..19}; do
-            printf -v ii '%02d' ${i}
-            ./bns "./netfiles/twoparam_${pp}_${q11}_${order}_${ii}_${i}.cnet" > "./attfiles/att_twoparam_${pp}_${q11}_${order}_${ii}_${i}.txt"
-            ./bns "./netfiles/twoparam_${pp}_1.00_${order}_${ii}_${i}.cnet" > "./attfiles/att_twoparam_${pp}_1.00_${order}_${ii}_${i}.txt"
-            ./bns "./netfiles/twoparam_0.00_${pp}_${order}_${ii}_${i}.cnet" > "./attfiles/att_twoparam_0.00_${pp}_${order}_${ii}_${i}.txt"
+        for ii in {00..19}; do
+            printf -v i '%d' "${ii#0}"
+            ./bns "./netfiles/twoparam_${pp}_${q11}_${order}_${ii}_${i}.cnet" > "./attfiles/att_twoparam_${pp}_${q11}_${order}_${ii}_${i}.txt" &
+            ./bns "./netfiles/twoparam_${pp}_1.00_${order}_${ii}_${i}.cnet" > "./attfiles/att_twoparam_${pp}_1.00_${order}_${ii}_${i}.txt" &
+            ./bns "./netfiles/twoparam_0.00_${pp}_${order}_${ii}_${i}.cnet" > "./attfiles/att_twoparam_0.00_${pp}_${order}_${ii}_${i}.txt" &
         done
         wait
     done
-    ./bns "./netfiles/twoparam_0.00_1.00_${order}_00_0.cnet" > "./attfiles/att_twoparam_0.00_1.00_${order}_00_0.txt"
-    ./bns "./netfiles/twoparam_1.00_0.00_${order}_00_0.cnet" > "./attfiles/att_twoparam_0.00_0.00_${order}_00_0.txt"
+    ./bns "./netfiles/twoparam_0.00_1.00_${order}_00_0.cnet" > "./attfiles/att_twoparam_0.00_1.00_${order}_00_0.txt" &
+    ./bns "./netfiles/twoparam_1.00_0.00_${order}_00_0.cnet" > "./attfiles/att_twoparam_0.00_0.00_${order}_00_0.txt" &
     wait
 done
