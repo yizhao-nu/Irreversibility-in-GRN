229995ac29881de6a926d199e18ff48d6448895b
twytock <twytock@gmail.com>
2023-12-07 23:41:09 -0600

    Confirming changes in attcsv files

--- irr_grn/attcsv.sh
+++ irr_grn/attcsv.sh
@@ -2,24 +2,17 @@ ps=(0.20 0.40 0.60 0.80)
 orders=('asc' 'desc')
 for order in ${orders[@]}; do
 for p in ${ps[@]}; do
-for i in {00..19}; do
+for ii in {00..19}; do
 q1=1.00
-printf -v ii "%02d" ${i}
+printf -v i "%d" ${ii#0}
 nohup python att_csv.py ./netfiles/twoparam_${p}_${q1}_${order}_${ii}_${i} > ./attfiles/att_twoparam_${p}_${q1}_${order}_${ii}_${i}.txt 2>&1 & 
-done
-wait
-for i in {00..19}; do
-q1=$(bc -l <<<"1-${p}")
-printf -v q11 "%.02f" ${q1}
-printf -v ii "%02d" ${i}
-nohup python3 att_csv.py ./netfiles/twoparam_${p}_${q11}_${order}_${ii}_${i}> ./attfiles/att_twoparam_${p}_${q11}_${order}_${ii}_{i}.txt 2>&1 & 
-done
-wait
 
-for i in {00..19}; do
-q1=0.00
-printf -v ii "%02d" ${i}
-nohup python att_csv.py ./netfiles/twoparam_${q1}_${p}_${order}_${ii}_${i} > ./attfiles/att_twoparam_${q1}_${p}_${order}_${ii}_${i}.txt 2>&1 & 
+q2=$(bc -l <<<"1-${p}")
+printf -v q11 "%.02f" ${q2}
+nohup python att_csv.py ./netfiles/twoparam_${p}_${q11}_${order}_${ii}_${i}> ./attfiles/att_twoparam_${p}_${q11}_${order}_${ii}_{i}.txt 2>&1 & 
+
+q3=0.00
+nohup python att_csv.py ./netfiles/twoparam_${q3}_${p}_${order}_${ii}_${i} > ./attfiles/att_twoparam_${q3}_${p}_${order}_${ii}_${i}.txt 2>&1 & 
 done
 wait
 
