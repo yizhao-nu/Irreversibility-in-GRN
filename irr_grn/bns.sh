c78b94cfa1a9ba7a1784d5cef57bb75042cf3116
twytock <38988877+twytock@users.noreply.github.com>
2024-07-17 12:55:10 -0500

    Update bns.sh
    Added the "pwait" function to ensure more efficient cpu usage.

--- irr_grn/bns.sh
+++ irr_grn/bns.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+cpus=8 ##$( ls -d /sys/devices/system/cpu/cpu[[:digit:]]* | wc -w ) ## can CHANGE to an integer to set the max number of processes
+script_name="bns"
+## Function to make the number of CPUs match the number of processes
+function pwait() {
+	while [ $(ps -u | grep -v "grep" | grep -c ${script_name}) -ge $1 ]; do
+        wait -n ## change to ""sleep 1"" if using bash version <4.3
+    done
+}
+
 ps=(0.20 0.40 0.60 0.80)
 ord=("asc" "desc")
 for order in ${ord[@]}; do
@@ -8,13 +18,13 @@ for order in ${ord[@]}; do
         printf -v pp '%.02f' ${p}
         for ii in {00..19}; do
             printf -v i '%d' "${ii#0}"
-            ./bns "./netfiles/twoparam_${pp}_${q11}_${order}_${ii}_${i}.cnet" > "./attfiles/att_twoparam_${pp}_${q11}_${order}_${ii}_${i}.txt" &
-            ./bns "./netfiles/twoparam_${pp}_1.00_${order}_${ii}_${i}.cnet" > "./attfiles/att_twoparam_${pp}_1.00_${order}_${ii}_${i}.txt" &
-            ./bns "./netfiles/twoparam_0.00_${pp}_${order}_${ii}_${i}.cnet" > "./attfiles/att_twoparam_0.00_${pp}_${order}_${ii}_${i}.txt" &
+            ./${script_name} "./netfiles/twoparam_${pp}_${q11}_${order}_${ii}_${i}.cnet" > "./attfiles/att_twoparam_${pp}_${q11}_${order}_${ii}_${i}.txt" &
+            ./${script_name} "./netfiles/twoparam_${pp}_1.00_${order}_${ii}_${i}.cnet" > "./attfiles/att_twoparam_${pp}_1.00_${order}_${ii}_${i}.txt" &
+            ./${script_name} "./netfiles/twoparam_0.00_${pp}_${order}_${ii}_${i}.cnet" > "./attfiles/att_twoparam_0.00_${pp}_${order}_${ii}_${i}.txt" &
+            pwait ${cpus}
         done
-        wait
     done
-    ./bns "./netfiles/twoparam_0.00_1.00_${order}_00_0.cnet" > "./attfiles/att_twoparam_0.00_1.00_${order}_00_0.txt" &
-    ./bns "./netfiles/twoparam_1.00_0.00_${order}_00_0.cnet" > "./attfiles/att_twoparam_0.00_0.00_${order}_00_0.txt" &
-    wait
+    ./${script_name} "./netfiles/twoparam_0.00_1.00_${order}_00_0.cnet" > "./attfiles/att_twoparam_0.00_1.00_${order}_00_0.txt" &
+    ./${script_name} "./netfiles/twoparam_1.00_0.00_${order}_00_0.cnet" > "./attfiles/att_twoparam_0.00_0.00_${order}_00_0.txt" &
+    pwait ${cpus}
 done
