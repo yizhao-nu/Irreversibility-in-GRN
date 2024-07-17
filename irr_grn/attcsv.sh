373c8fc86e68805c4a2d370a1ec911bc5b1b576a
twytock <38988877+twytock@users.noreply.github.com>
2024-07-17 14:04:53 -0500

    Updated attcsv.sh to include the pwait function

--- irr_grn/attcsv.sh
+++ irr_grn/attcsv.sh
@@ -1,3 +1,11 @@
+cpus=8 ##$( ls -d /sys/devices/system/cpu/cpu[[:digit:]]* | wc -w ) ## can CHANGE to an integer to set the max number of processes
+script_name="att_csv.py"
+## Function to make the number of CPUs match the number of processes
+function pwait() {
+	while [ $(ps -u | grep -v "grep" | grep -c ${script_name}) -ge $1 ]; do
+        wait -n ## change to ""sleep 1"" if using bash version <4.3
+    done
+}
 ps=(0.20 0.40 0.60 0.80)
 ord=('asc' 'desc')
 for order in ${ord[@]}; do
@@ -5,18 +13,19 @@ for p in ${ps[@]}; do
 for ii in {00..19}; do
 q1=1.00
 printf -v i "%d" ${ii#0}
-nohup python att_csv.py ./netfiles/twoparam_${p}_${q1}_${order}_${ii}_${i} > ./attfiles/1stOP_twoparam_${p}_${q1}_${order}_${ii}_${i}.txt 2>&1 & 
-
+nohup python ${script_name} ./netfiles/twoparam_${p}_${q1}_${order}_${ii}_${i} > ./attfiles/1stOP_twoparam_${p}_${q1}_${order}_${ii}_${i}.txt 2>&1 & 
+pwait ${cpus}
 q2=$(bc -l <<<"1-${p}")
 printf -v q11 "%.02f" ${q2}
-nohup python att_csv.py ./netfiles/twoparam_${p}_${q11}_${order}_${ii}_${i}> ./attfiles/1stOP_twoparam_${p}_${q11}_${order}_${ii}_${i}.txt 2>&1 & 
-
+nohup python ${script_name} ./netfiles/twoparam_${p}_${q11}_${order}_${ii}_${i}> ./attfiles/1stOP_twoparam_${p}_${q11}_${order}_${ii}_${i}.txt 2>&1 & 
+pwait ${cpus}
 q3=0.00
-nohup python att_csv.py ./netfiles/twoparam_${q3}_${p}_${order}_${ii}_${i} > ./attfiles/1stOP_twoparam_${q3}_${p}_${order}_${ii}_${i}.txt 2>&1 & 
+nohup python ${script_name} ./netfiles/twoparam_${q3}_${p}_${order}_${ii}_${i} > ./attfiles/1stOP_twoparam_${q3}_${p}_${order}_${ii}_${i}.txt 2>&1 & 
 done
-wait
+pwait ${cpus}
 
 done
-nohup python att_csv.py ./netfiles/twoparam_0.00_1.00_${order}_00_0> ./attfiles/1stOP_twoparam_0.00_1.00_${order}_00_0.txt 2>&1 &
-nohup python att_csv.py ./netfiles/twoparam_1.00_0.00_${order}_00_0> ./attfiles/1stOP_twoparam_1.00_0.00_${order}_00_0.txt 2>&1 &
+nohup python ${script_name} ./netfiles/twoparam_0.00_1.00_${order}_00_0> ./attfiles/1stOP_twoparam_0.00_1.00_${order}_00_0.txt 2>&1 &
+nohup python ${script_name} ./netfiles/twoparam_1.00_0.00_${order}_00_0> ./attfiles/1stOP_twoparam_1.00_0.00_${order}_00_0.txt 2>&1 &
+pwait ${cpus}
 done
