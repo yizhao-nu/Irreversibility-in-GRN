879246ed6f7ed51c4b57e5b93cfddc1162d701d7
twytock <38988877+twytock@users.noreply.github.com>
2024-07-18 11:29:15 -0500

    Added file to list (again)

--- irr_grn/gather_chunked_files.py
+++ irr_grn/gather_chunked_files.py
@@ -10,7 +10,8 @@ def main():
         'twoparam_0.40_1.00_asc_00_0', 'twoparam_0.40_1.00_asc_02_2', 'twoparam_0.40_1.00_asc_03_3', 
         'twoparam_0.40_1.00_asc_05_5', 'twoparam_0.40_1.00_asc_08_8', 'twoparam_0.40,_1.00_asc_09_9', 
         'twoparam_0.40_1.00_asc_10_10', 'twoparam_0.40_1.00_asc_11_11', 'twoparam_0.40_1.00_asc_17_17', 
-        'twoparam_0.80_1.00_asc_07_7', 'twoparam_0.80_1.00_asc_01_1', 'twoparam_0.60_1.00_asc_16_16', 'twoparam_0.40_1.00_asc_18_18' ]
+        'twoparam_0.80_1.00_asc_07_7', 'twoparam_0.80_1.00_asc_01_1', 'twoparam_0.60_1.00_asc_16_16', 
+        'twoparam_0.40_1.00_asc_18_18', 'twoparam_0.40_1.00_asc_09_9']
 
 
     prefix_l = ['changed','result-OE','result-KO']
